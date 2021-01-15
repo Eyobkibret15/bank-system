bd671932b7566429aa9572e6af8256f66562b621
eyob <ayaleweyob15@gmail.com>
2021-01-15 17:18:24 +0100

    final class complit code

--- Program.cs
+++ Program.cs
@@ -6,8 +6,34 @@ namespace bank_system
     {
         static void Main(string[] args)
         {
-            Bank b = new Bank();
-            b.Card_generator();
+            Console.WriteLine("*******WELCOME********");
+            menu();
         }
+        public static void menu()
+        {
+            Bank bank_obj = new Bank();
+            Console.WriteLine("1. Creat an account \n2. Log into account \n3. Exit");
+            Console.WriteLine("please enter your choice number:");
+            string choice = Console.ReadLine();
+            if (choice == "1")
+            {
+                bank_obj.Creat_Account();
+            }
+            else if (choice == "2")
+            {
+                bank_obj.Logged_in();
+            }
+            else if (choice == "3")
+            {
+                Environment.Exit(1);
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice  please enter valid choice number:");
+            }
+            Console.WriteLine();
+            menu();
+        }
+
     }
 }
